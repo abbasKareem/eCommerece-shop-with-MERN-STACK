ee38e665627fa0ee36ee6068390c3df2bf93fc18
abbas <kareemabbas106@gmail.com>
2021-02-12 19:08:31 +0200

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === 'development') {
 // I have to add this line to get the data in req.body in post requsts
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API running...')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -32,6 +28,18 @@ app.use('/api/upload', uploadRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running...')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
